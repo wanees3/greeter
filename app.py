530beb8968e8206bf5625e2a821c50ff4372e841
Warda Anees <wardaanees123@gmail.com>
2026-09-19 14:10:12 +0500

    Initial greeter app

---
+++ app.py
@@ -0,0 +1,26 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class GreeterHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Greeter</title>
+        </head>
+        <body>
+            <h1>Hello, Warda!</h1>
+        </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+server = HTTPServer(("0.0.0.0", 8080), GreeterHandler)
+
+print("Greeter running on http://localhost:8080")
+
+server.serve_forever()
